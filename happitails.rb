69d69c7928b00fcca2d581d8b73928f2914150b4
Wesley Carr <surfswell17@gmail.com>
2013-06-21 00:36:57 -0400

    Fixed ID's to show full client attributes(to_s)

--- happitails.rb
+++ happitails.rb
@@ -8,6 +8,10 @@ class Client
     @num_of_dogs = num_of_dogs
   end
 
+    def to_s
+        "#{@name}, #{@gender}: #{@age} year-old with #{@num_kids} kids and #{@num_of_dogs} dogs"
+    end
+
   def give_dog(name, breed, age, gender, favorite_toy)
     puts "#{name}, a #{age} year-old #{breed} entered HappiTails\n\n"
     return Dog.new(name, breed, age, gender, favorite_toy)
@@ -46,9 +50,9 @@ adoptable_dogs = []
 list_clients = []
 
 #Shelter has a few clients already
-list_clients << Client.new("Kevon", 23, "male", 0, 1)
-list_clients << Client.new("Sarah", 20, "female", 0, 0)
-list_clients << Client.new("Brenda", 30, "female", 2, 2)
+list_clients << Client.new("Kevon", 23, "Male", 0, 1)
+list_clients << Client.new("Sarah", 20, "Female", 0, 0)
+list_clients << Client.new("Brenda", 30, "Female", 2, 2)
 
 #Shelter has a few dogs
 adoptable_dogs << Dog.new("Toto", "Doberman", 4, "Male", "Grass")
@@ -59,7 +63,7 @@ puts "\nHere are our current dogs up for adoption~ "
 list_dogs(adoptable_dogs)
 
 #need this to allow kevon to give dog to adoptable_dogs
-kevon = Client.new("Kevon", 23, "male", 0, 1)
+kevon = Client.new("Kevon", 23, "Male", 0, 1)
 
 puts "\nOh wait! Kevon is giving his dog to the shelter..."
 adoptable_dogs << kevon.give_dog("Momo", "Poodle", 3, "Female", "Tennis Ball")
